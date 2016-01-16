3c2e35414aae15b15770460a765a1c6b001af53f
edpete <edpete@gmail.com>
2016-01-16 12:19:03 -0800

    Initial Assets

---
+++ Scripts/DestroyScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyScript : MonoBehaviour 
+{
+	public float destroyTime = 3.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+    	Destroy (this.gameObject, destroyTime);	
+	}
+}
---
+++ Scripts/SpawnItems.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnItems : MonoBehaviour 
+{
+
+	public Transform[] SpawnPoints;
+	public float spawnTime = 1.5f; 
+
+	public GameObject kaiju; 
+	//public GameObject[] kaiju;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating ("SpawnKaiju", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnKaiju()
+	{
+		//DestroyScript is killing the object so... Need to clean this up. 
+		if (kaiju != null) 
+		
+		{
+			int spawnIndex = Random.Range (0, SpawnPoints.Length);
+			Instantiate (kaiju, SpawnPoints [spawnIndex].position, SpawnPoints [spawnIndex].rotation);
+		}
+
+	}
+}
